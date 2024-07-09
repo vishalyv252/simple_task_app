e6d41501b890d0af2cd9e89768d2ccba8cd75562
vishalyv252 <vishalyella@gmail.com>
2024-07-09 16:48:55 +0530

    Finished Project.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { NotFoundPageComponent } from './components/not-found-page/not-found-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: '**', component: NotFoundPageComponent }
+];
--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponentComponent } from './nested_components/header-component/header-component.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponentComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
